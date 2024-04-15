3f98d331507ee1d94e7d67826c7e303861d8bb68
qining <7393990+qining_fan@user.noreply.gitee.com>
2024-04-15 15:30:50 +0800

    Transformation详解

--- flink-ademo/src/main/java/com/source/demo/SocketWordCountStreamGraph.java
+++ flink-ademo/src/main/java/com/source/demo/SocketWordCountStreamGraph.java
@@ -16,28 +16,34 @@ import org.apache.flink.util.Collector;
  * @Version: 1.0
  */
 
-/**
-  * @授课老师(V): yi_locus
-  * email: 156184212@qq.com
- * @Date: Created in 2024/4/14 1:51
- * @Version: 1.0
-  */
-public class SocketWordCount {
+
+public class SocketWordCountStreamGraph {
     public static void main(String[] args) throws Exception{
         /**
          * 创建StreamExecutionEnvironment
          */
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.registerCachedFile("F:/a.txt","cache");
+        /** 设置检查点的时间间隔 */
+        //env.enableCheckpointing(5000);
+        /** 设置检查点路径*/
+        env.getCheckpointConfig().setCheckpointStorage("file:///source/chk");
+        env.setParallelism(5);
+        env.setMaxParallelism(10);
         /** 读取socket数据 */
         DataStreamSource<String> fileStream =   env.socketTextStream("127.0.0.1",9999);
         /** 将数据转成小写 */
         SingleOutputStreamOperator<String> mapStream = fileStream.map(String :: toUpperCase);
+        mapStream.setDescription("aaaaaaa");
         /** 按照空格切分字符串*/
         SingleOutputStreamOperator<Tuple2<String,Integer>> flatMapStream = mapStream.flatMap(new Split());
+        mapStream.setDescription("bbbbb");
+
         /** 分组聚合*/
         KeyedStream<Tuple2<String,Integer>,String> keyStream = flatMapStream.keyBy(value -> value.f0);
         /** 聚合*/
         SingleOutputStreamOperator<Tuple2<String,Integer>> sumStream = keyStream.sum(1);
+        //sumStream.cache();
         /** 打印 */
         DataStreamSink<Tuple2<String,Integer>> sink = sumStream.print();
         /** 执行任务 */
